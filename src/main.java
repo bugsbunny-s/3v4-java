a3f9a61800215f8b892e4a587b3dd2678a6ab638
Bugra Tosun <bugratosun112@gmail.com>
2023-02-01 21:33:45 +0300

    3ve4

--- src/main.java
+++ src/main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) throws Exception {
-        int a, i = 1;
+        int a, toplam=0, i=1;
+        double j=0;
         Scanner inp = new Scanner(System.in);
         System.out.print("Sayı Giriniz : ");
         a = inp.nextInt();
@@ -9,8 +10,11 @@ public class main {
         while(i <= a){
             if((i % 3 == 0) && (i % 4== 0)){
                 System.out.println(i);
+                toplam +=i;
+                j++;
             }
             i++;
         }
+        System.out.println("3 ve 4 e tam bölünen sayıların ortalaması : " + toplam / j);
     }
 }
